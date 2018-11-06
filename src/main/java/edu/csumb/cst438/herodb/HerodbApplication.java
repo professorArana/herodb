eda40a7bc03267075b5e38a9a04ce38f5a6d7fd9
professorArana <31639021+professorArana@users.noreply.github.com>
2018-11-06 14:54:03 -0800

    prod candidate init

--- src/main/java/edu/csumb/cst438/herodb/HerodbApplication.java
+++ src/main/java/edu/csumb/cst438/herodb/HerodbApplication.java
@@ -2,9 +2,7 @@ package edu.csumb.cst438.herodb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-@EnableEurekaClient
 @SpringBootApplication
 public class HerodbApplication {
 
